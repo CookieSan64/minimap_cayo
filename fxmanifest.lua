9e136fddbc1a229b967abe96bac59220deb176a1
ChouCookieSan <choucookiesan@gmail.com>
2024-03-08 02:27:21 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+author "ChouCookieSan"
+description "Map Cayo en couleur"
+version "1.0.0"
+
+fx_version "cerulean"
+game "gta5"
+
+this_is_a_map 'yes'
+
+data_file "SCALEFORM_DLC_FILE" "stream/int3232302352.gfx"
+
+files {
+  "stream/int3232302352.gfx"
+}
